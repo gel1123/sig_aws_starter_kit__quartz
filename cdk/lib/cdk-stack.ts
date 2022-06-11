97729955944c081583b096e19bdd60f7f5a9fa74
gel1123 <41170561+gel1123@users.noreply.github.com>
2022-06-12 07:55:21 +0900

    wip

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,36 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_cognito-readme.html
+    const userPool = new cognito.UserPool(this, 'quartz_userpool', {
+      userPoolName: 'quartz_app-userpool',
+      signInAliases: {
+        username: true,
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+    });
+    userPool.addClient('quartz_app-userpool-client', {
+      userPoolClientName: 'quartz_app-userpool-client',
+      generateSecret: true,
+      accessTokenValidity: Duration.hours(1),
+      idTokenValidity: Duration.hours(1),
+      authFlows: {
+        userPassword: true,
+      }
+    });
+    userPool.addDomain('quartz_app-userpool-domain', {
+      cognitoDomain: {
+        domainPrefix: 'quartz',
+      },
+    });
+    
   }
 }
