c889eda32c83fdf3eadc0bb1244fff0438e49295
shigeru.hyodo <41170561+gel1123@users.noreply.github.com>
2022-06-17 08:44:01 +0900

    feat: add nuxt3 + tailwindcss

---
+++ nuxt3/README.md
@@ -0,0 +1,3 @@
+## 参考サイト
+* https://std9.jp/articles/01fq14pxsp4r6nbyzp2txj46ac
+
---
+++ nuxt3/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p class="p-10 w-full text-xl bg-orange-300 hover:opacity-70">Hello World!</p>
+  </div>
+</template>
