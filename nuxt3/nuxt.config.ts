ed1e04920ff238127799001c377edca336c20d8d
gel1123 <41170561+gel1123@users.noreply.github.com>
2022-06-18 06:37:54 +0900

    feat: update nuxt.config.ts

--- nuxt3/nuxt.config.ts
+++ nuxt3/nuxt.config.ts
@@ -2,11 +2,24 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  nitro: {
+      preset: 'lambda' //<= Nuxt3をLambdaアプリケーションとして構成する設定
+  },
+  typescript: {
+      strict: true
+  },
+  privateRuntimeConfig: {
+      threadsTableName: process.env.THREADS_TABLE_NAME,
+      region: process.env.REGION,
+      mode: process.env.MODE,
+      bucket: process.env.BUCKET,
+      domain: process.env.DOMAIN
+  },
+  ssr: true, //<= ビルドモードの指定。trueなら SSR or SSG. falseなら SPA. デフォルトでtrue.
+  target: 'server', //<= ビルドモードの指定。'server'なら SSR. 'static'なら SSG or SPA. デフォルトで 'server'.
   css: [
     '@/assets/css/main.css',
   ],
-
   build: {
     postcss: {
       postcssOptions: {
