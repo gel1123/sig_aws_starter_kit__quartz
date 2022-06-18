538fad0b5ad7e894af3e73a147745175b63f1461
gel1123 <41170561+gel1123@users.noreply.github.com>
2022-06-18 14:16:20 +0900

    wip

--- nuxt3/server/api/echo.ts
+++ nuxt3/server/api/echo.ts
@@ -1,21 +1,12 @@
-import type { IncomingMessage, ServerResponse } from 'http'
 
-// /**
-//  * ```
-//  * curl -H "Content-Type: application/json" -XPOST -d '{"message":"Hello World"}' http://localhost:3000/api/echo
-//  * ```
-//  */
-// const mainProcedure = ({body, res}: {
-//   body?: string,
-//   res: ServerResponse
-// }) => {
-//   const result = {body};
-//   res.writeHead(200, { "Content-Type": "application/json" });
-//   res.write(JSON.stringify(result));
-//   res.end();
-// };
-
-// https://v3.nuxtjs.org/guide/features/server-routes/
+/**
+ * https://v3.nuxtjs.org/guide/features/server-routes/
+ * 
+ * ```
+ * curl -H "Content-Type: application/json" -XPOST -d '{"message":"Hello World"}' http://localhost:3000/api/echo
+ * curl -H "Content-Type: application/json" -XPOST -d '{"message":"Hello World"}' https://d31y3mgphorb7z.cloudfront.net/api/echo
+ * ```
+ */
 export default defineEventHandler(async (e) => {
   const body = await useBody(e);
   const query = await useQuery(e);
@@ -27,20 +18,4 @@ export default defineEventHandler(async (e) => {
     })
   }
   return {body, query}
-
-
-  // if (req.method !== "POST") {
-  //   mainProcedure({res});
-
-  // } else if ((req as any).body) {
-  //   const body = (req as any).body;
-  //   mainProcedure({body, res});
-
-  // } else {
-  //   let body = "";
-  //   req.on("data", chunkData => { body += chunkData; });
-  //   req.on("end", async () => {
-  //     mainProcedure({body, res});
-  //   });
-  // }
 });
