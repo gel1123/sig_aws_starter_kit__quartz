b3160ab215e217bcadd08622bb02a618ffa44d7a
gel1123 <41170561+gel1123@users.noreply.github.com>
2022-06-18 21:19:45 +0900

    wip

--- nuxt3/server/middleware/auth.ts
+++ nuxt3/server/middleware/auth.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/guide/features/server-routes
 export default defineEventHandler((event) => {
-  if (event.req.url === '/') {
+  if (!event.req.url?.startsWith('/api/')) {
     event.res.writeHead(302, {
       Location: 'https://quartz.auth.ap-northeast-1.amazoncognito.com/login?client_id=bo73u1ihm98ttrqe5dfkolq7d&redirect_uri=http://localhost:3000&response_type=code'
     });
