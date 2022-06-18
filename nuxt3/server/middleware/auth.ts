404e191f413f33aa2756d7bd2ba48ee3404c5736
gel1123 <41170561+gel1123@users.noreply.github.com>
2022-06-18 20:47:12 +0900

    feat: redirect to login endpoint (cognito)

--- nuxt3/server/middleware/auth.ts
+++ nuxt3/server/middleware/auth.ts
@@ -1,4 +1,9 @@
 // https://v3.nuxtjs.org/guide/features/server-routes
 export default defineEventHandler((event) => {
-  console.log('New request: ' + event.req.url);
+  if (event.req.url === '/') {
+    event.res.writeHead(302, {
+      Location: 'https://quartz.auth.ap-northeast-1.amazoncognito.com/login?client_id=bo73u1ihm98ttrqe5dfkolq7d&redirect_uri=http://localhost:3000&response_type=code'
+    });
+    event.res.end();
+  }
 })
