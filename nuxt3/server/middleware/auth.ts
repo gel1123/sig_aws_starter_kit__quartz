0d352e67aa7b7e6202c4fd42f5ea62399a772b87
gel1123 <41170561+gel1123@users.noreply.github.com>
2022-06-19 07:49:39 +0900

    wip

--- nuxt3/server/middleware/auth.ts
+++ nuxt3/server/middleware/auth.ts
@@ -5,13 +5,15 @@ export default defineEventHandler((event) => {
   const code = query.code;
 
   if (code) {
-    // 認証コードをトークンエンドポイントにPOSTする
-    console.log({code});
     // Base64Encode(client_id:client_secret) を生成する
     const clientId = "bo73u1ihm98ttrqe5dfkolq7d";
-    const clientSecret = code;
+    const clientSecret = "8j43e02bdvv3hf683uag0dti3ch249tdfiqa4dv6dn1td051skd";
     const clientIdSecret = `${clientId}:${clientSecret}`;
     const clientIdSecretBase64 = Buffer.from(clientIdSecret).toString('base64');
+
+    console.log({code, clientIdSecretBase64});
+
+    // 認証コードをトークンエンドポイントにPOSTする
     const tokenEndpoint = "https://quartz.auth.ap-northeast-1.amazoncognito.com/oauth2/token";
     fetch(tokenEndpoint, {
       method: "POST",
@@ -19,12 +21,10 @@ export default defineEventHandler((event) => {
         "Content-Type": "application/x-www-form-urlencoded",
         "Authorization": `Basic ${clientIdSecretBase64}`,
       },
-      body: `
-        grant_type=authorization_code&
-        client_id=bo73u1ihm98ttrqe5dfkolq7d&
-        redirect_uri=localhost:3000&
-        code=${code}
-      `
+      body: "grant_type=authorization_code&" +
+      "client_id=bo73u1ihm98ttrqe5dfkolq7d&" +
+      "redirect_uri=localhost:3000&" +
+      `code=${code}`
     }).then(res => {
       const json = res.json();
       return json;
