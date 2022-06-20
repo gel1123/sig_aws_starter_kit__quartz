455e03bbea1f4da9d5db9e0fb2d9ab9aa178ff71
gel1123 <41170561+gel1123@users.noreply.github.com>
2022-06-21 06:37:22 +0900

    wip

--- nuxt3/server/api/echo.ts
+++ nuxt3/server/api/echo.ts
@@ -1,5 +1,11 @@
 import { CognitoJwtVerifier } from "aws-jwt-verify";
 
+const verifier = CognitoJwtVerifier.create({
+  userPoolId: "ap-northeast-1_4LMZhoi0a",
+  tokenUse: "access",
+  clientId: "bo73u1ihm98ttrqe5dfkolq7d",
+});
+
 /**
  * https://v3.nuxtjs.org/guide/features/server-routes/
  * 
@@ -16,11 +22,6 @@ export default defineEventHandler(async (e) => {
   const body =  e.req.method === 'POST' ? await useBody<string>(e) : undefined;
   const query = useQuery(e);
   const access_token = useCookies(e).access_token;
-  const verifier = CognitoJwtVerifier.create({
-    userPoolId: "ap-northeast-1_4LMZhoi0a",
-    tokenUse: "access",
-    clientId: "bo73u1ihm98ttrqe5dfkolq7d",
-  });
   try {
     // トークン検証（失敗すれば例外が発生する。例外なしなら、検証成功）
     await verifier.verify(access_token);
