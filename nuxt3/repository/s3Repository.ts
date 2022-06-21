72ef35347961a44a647dd42851024565dfa79528
shigeru.hyodo <41170561gel1123@users.noreply.github.com>
2022-06-21 23:13:52 +0900

    wip

--- nuxt3/repository/s3Repository.ts
+++ nuxt3/repository/s3Repository.ts
@@ -1,1 +1,14 @@
-//TODO
+import { S3Client } from '@aws-sdk/client-s3';
+
+let memo = {} as { client?: S3Client};
+
+export const getS3Client = () => {
+  if (memo.client) return memo.client;
+
+  const config = useRuntimeConfig();
+  const s3Client = new S3Client({
+    region: config.region,
+  });
+  memo.client = s3Client;
+  return s3Client;
+};
