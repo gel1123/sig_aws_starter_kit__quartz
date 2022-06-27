c4aacd8f2ee53c69e0f5c30eb46bd0208e53ee47
shigeru.hyodo <41170561gel1123@users.noreply.github.com>
2022-06-27 10:25:32 +0900

    feat: スタック名を環境ごとに生成するなど

--- cdk/lib/role-stack.ts
+++ cdk/lib/role-stack.ts
@@ -18,14 +18,13 @@ export class RoleStack extends Stack {
      * なおメインスタックで new Role() し、edgeFunction に渡すと、
      * 循環参照エラーが生じてしまう。
      * 
-     * （関連Issue: ）
+     * （関連Issue: https://github.com/aws/aws-cdk/issues/13236）
      * 
      * これを解消するために、グローバルリソースであるところのIAMロールを、
      * 独立した本スタックにて定義し、その後で edgeFunction が生成する暗黙的なスタックと、
      * メインスタックを生成している。
      */
-    const lambdaEdgeRole = new Role(this, "QuartzOperateS3Role", {
-      roleName: "quartzLambdaEdgeRole",
+    const lambdaEdgeRole = new Role(this, `${id}_OperateS3Role`, {
       assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
       managedPolicies: [
         ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")
