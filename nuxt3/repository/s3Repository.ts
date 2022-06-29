97ef6568aab30625844cabc086384677b516f9ab
shigeru.hyodo <41170561gel1123@users.noreply.github.com>
2022-06-29 16:00:50 +0900

    これも動く（クレデンシャルのパッケージimport済み）

--- nuxt3/repository/s3Repository.ts
+++ nuxt3/repository/s3Repository.ts
@@ -14,7 +14,7 @@ import { S3Client } from '@aws-sdk/client-s3';
 // 参考：
 //  - https://stackoverflow.com/questions/70060570/vitesse-vue3-issue-adding-libraries
 //  - https://github.com/ionic-team/rollup-plugin-node-polyfills
-// import { CognitoIdentityCredentialProvider, fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
+import { CognitoIdentityCredentialProvider, fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
 
 let memo = {} as { client?: S3Client};
 
@@ -34,19 +34,19 @@ export const getS3Client = ({region, frontEndOption}: {
    */
   const credentials = (() => {
 
-    //TODO Viteとの噛み合わせがとても悪いので、現在はundefinedのみ
-    return undefined;
+    // //TODO Viteとの噛み合わせがとても悪いので、現在はundefinedのみ
+    // return undefined;
 
-    // if (!frontEndOption) return undefined;
-    // const {identityPoolId, idToken} = frontEndOption;
-    // const credentialProvider: CognitoIdentityCredentialProvider = fromCognitoIdentityPool({
-    //   identityPoolId: identityPoolId,
-    //   logins: {
-    //     "www.amazon.com": idToken,
-    //   },
-    //   clientConfig: {region: region},
-    // });
-    // return credentialProvider;
+    if (!frontEndOption) return undefined;
+    const {identityPoolId, idToken} = frontEndOption;
+    const credentialProvider: CognitoIdentityCredentialProvider = fromCognitoIdentityPool({
+      identityPoolId: identityPoolId,
+      logins: {
+        "www.amazon.com": idToken,
+      },
+      clientConfig: {region: region},
+    });
+    return credentialProvider;
   })()
 
   const s3Client = new S3Client({
