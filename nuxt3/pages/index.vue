4006e986bcb01e6c2d8601dca0c92721ed7fc40f
shigeru.hyodo <41170561+gel1123@users.noreply.github.com>
2022-06-30 16:03:39 +0900

    DynamoDBからレコードを読んで、取得したパスの画像を表示するよう実装

--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -4,18 +4,13 @@ import { resize } from '~~/usecase/imageUsecase';
 const fetchedData = ref<undefined | string>(undefined);
 const fetch = async () => {
   console.log('clicked');
-  const res = await useFetch('/api/echo', {
+  const res = await $fetch('/api/echo', {
     method: 'POST',
     body: {
       message: 'Hello World'
     },
   });
-  if (res.error.value) {
-    console.error(res.error.value);
-  }
-  const data = res.data.value.body;
-  console.log({data});
-  fetchedData.value = data;
+  fetchedData.value = JSON.stringify(res);
 };
 
 const resizedDataUrl = ref<null | string>(null);
@@ -34,20 +29,37 @@ const onChangeImage = async (e: Event) => {
   }
 };
 
+
+const itemID = "test";
+
 const uploadImage = async () => {
   const dataUrl = resizedDataUrl.value;
   const result = await useFetch('/api/putImage', {
     method: "POST",
-    body: {dataUrl, fileName: fileName.value},
+    body: {dataUrl, fileName: fileName.value, itemID},
   });
   if (result.error.value) {
     alert("通信に失敗しました。時間をおいて再度お試しください。")
   }
   console.log("result.data: ", result.data);
   alert("画像のアップロードが完了しました。");
+  url.value = (await $fetch("/api/getItemRecord", {
+    params: {itemID}
+  })).Item.imagePath;
 };
 
-const url = "/items/test/item001.imagefile";
+// GET なら RefとしてTemplateで使う前提の useFetch より、$fetchの方が良い。
+// useFetch() は非同期Refであり、Ref.valueを介してテンプレート外で値を取得しようとすると、
+// nullになってしまうケースがある。（非同期で値が変わるが、await で待つようなことはできない）。
+// 
+// 一方で $fetch は直接APIエンドポイントを叩くシンプルなAPI。
+// https://v3.nuxtjs.org/guide/features/data-fetching#directly-calling-an-api-endpoint
+const res = await $fetch("/api/getItemRecord", {
+  params: {itemID}
+});
+console.log("res: ", res);
+
+const url = ref<string | undefined>(res.Item.imagePath);
 
 </script>
 <template>
@@ -88,6 +100,7 @@ const url = "/items/test/item001.imagefile";
       </button>
 
       <img :src="url" class="my-4" />
+      <pre>{{data}}</pre>
     </Outline>
     <Outline>
       <p>If you want to log out, press the button below.</p>
